726bfd7a1157624e29c7fbe51309e83c281f6944
greycanvas <sixletters1970@hotmail.com>
2021-10-09 18:51:50 -0400

    Update Processing

--- src/assets/draw.js
+++ src/assets/draw.js
@@ -1,14 +1,8 @@
 function updateCanvas() {
-   y = 150;
-   x = 0;
-   red = 100;
-   blue = 177;
-   green = 156;
-  }
+   szMult = 1.5;
+   rec1 = -40;
+}
 function updateCanvas2(){
-   y = 300;
-   x = 0;
-   red = 234;
-   blue = 77;
-   green = 56;
+   szMult = 4.5;
+   rec1 = -30;
 }
